454c67d3a839bbbfea3558fc0ce829c2d8934349
Eshaan Bansal <eshaanbansal2005@gmail.com>
2026-02-24 12:34:35 +0530

    API CALLING FROM CLI

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const dotenv = require('dotenv').config()
-const command = require('commander')
+const { Command } = require('commander')
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
-const program = new command();
+const TMDB_RAT = process.env.TMDB_RAT;
+const program = new Command();
 
 console.log('Hi from CLI')
 
@@ -16,5 +17,20 @@ program.command('search')
     .argument('<title>', 'Title of the movie')
     .option('-y --year<number>', "release year")
     .action((title, options) => {
-        console.log('seraching for title')
+        console.log('seraching for', title)
+        const url = `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`;
+        let api_options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${TMDB_RAT}`
+            }
+        };
+
+        fetch(url, api_options)
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .catch(err => console.error(err));
     })
+
+program.parse()
