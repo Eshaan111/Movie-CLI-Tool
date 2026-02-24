8cbbe438394465d7318e558aa6a93a0488e2af06
Eshaan Bansal <eshaanbansal2005@gmail.com>
2026-02-24 12:18:16 +0530

    ENV SETUP

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
+const dotenv = require('dotenv').config()
+const command = require('commander')
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const program = new command();
 
 console.log('Hi from CLI')
+
+program
+    .name('Movie-CLI')
+    .description('A CLI TO GET MOVIES FROM APIs')
+    .version('1.0.0')
+
+program.command('search')
+    .description('Get Movie Details By Name')
+    .argument('<title>', 'Title of the movie')
+    .option('-y --year<number>', "release year")
+    .action((title, options) => {
+        console.log('seraching for title')
+    })
